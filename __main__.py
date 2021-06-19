433b5eb6d78954d174e08bc28c9fa880aacfd38d
cyber-jabuticaba <rafael.becker.winck@gmail.com>
2021-06-18 23:20:13 -0300

    idk why it isnt fetching the tweets

--- __main__.py
+++ __main__.py
@@ -1,9 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL  = 'https://twitter.com/?lang=en'
-PAGE = requests.get(URL)
-SOUP = BeautifulSoup(PAGE.content, 'html.parser')
+MAX = 10
+URL = 'https://twitter.com/search'
 
 if __name__ == '__main__':
-    print(SOUP.prettify())
+    # only works with simple words like 'Banana' or 
+    # 'hot dogs'.
+    query = input('> ').replace(' ', '%20')
+    URL = URL + '?q=' + query
+
+    print('going to: %s' % URL)
+
+    PAGE = requests.get(URL)
+    SOUP = BeautifulSoup(PAGE.content, 'html.parser')
+
+    i = 0
+    for d in SOUP.find_all('div'):
+        if d.get('data-testid') == 'tweet':
+            print('TWEET #%s ================' % i)
+            print(d.prettify())
+            print('==========================')
+            i += 1
+
+            if d == MAX:
+                exit(0)
+    else:
+        print('nothing')
