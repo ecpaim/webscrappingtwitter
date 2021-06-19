405edc84ce4af3b0c9f72fbccc7d8c758f79897f
cyber-jabuticaba <rafael.becker.winck@gmail.com>
2021-06-19 20:53:37 -0300

    Acho que essa é a search bar

--- __main__.py
+++ __main__.py
@@ -5,8 +5,6 @@ MAX = 10
 URL = 'https://twitter.com/search'
 
 if __name__ == '__main__':
-    # only works with simple words like 'Banana' or 
-    # 'hot dogs'.
     query = input('> ').replace(' ', '%20')
     URL = URL + '?q=' + query
 
@@ -15,15 +13,4 @@ if __name__ == '__main__':
     PAGE = requests.get(URL)
     SOUP = BeautifulSoup(PAGE.content, 'html.parser')
 
-    i = 0
-    for d in SOUP.find_all('div'):
-        if d.get('data-testid') == 'tweet':
-            print('TWEET #%s ================' % i)
-            print(d.prettify())
-            print('==========================')
-            i += 1
-
-            if d == MAX:
-                exit(0)
-    else:
-        print('nothing')
+    print(SOUP.prettify())
--- latest10.py
+++ latest10.py
@@ -13,22 +13,23 @@ opts.headless = True
 assert opts.headless
 browser = Firefox(options=opts)
 
-browser.get('https://duckduckgo.com')
-search = browser.find_element_by_id('search_form_input_homepage')
+browser.get('https://twitter.com/explore/')
+search = browser.find_elements_by_xpath("//input[@aria-label='Search query']")
 search.send_keys("testing")
 search.submit()
+print(search)
 
-# wait for URL to change with 15 seconds timeout
-WebDriverWait(browser, 15).until(EC.url_changes(browser.current_url))
+#wait for URL to change with 10 seconds timeout
+WebDriverWait(browser, 10).until(EC.url_changes(browser.current_url))
 print(browser.current_url)
 
-results = WebDriverWait(browser,10).until(
-        EC.presence_of_element_located((By.ID, "links"))
-    )
-time.sleep(5)
-results = browser.find_elements_by_css_selector('.result.results_links_deep.highlight_d.result--url-above-snippet')
-print(results[0].text)
-
+#results = WebDriverWait(browser,5).until(
+#       EC.presence_of_element_located((By.ID, "links"))
+#    )
+#    
+#time.sleep(5)
+#results = browser.find_elements_by_css_selector('.result.results_links_deep.highlight_d.result--url-above-snippet')
+#print(results[0].text)
 
 browser.close()
 quit()
