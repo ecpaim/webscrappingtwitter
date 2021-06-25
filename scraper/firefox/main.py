785c65eb9bfb408854bdaebb8626e284312ae74f
cyber-jabuticaba <rafael.becker.winck@gmail.com>
2021-06-25 17:31:56 -0300

    minor

--- scraper/firefox/main.py
+++ scraper/firefox/main.py
@@ -10,37 +10,36 @@ try:
     while True:
         opt = Options()
         opt.headless = True
-        driver = webdriver.Firefox(options=opt)
-        query = input('enter query> ').replace(' ', '%20')
-        driver.get('https:/twitter.com/search?q=' + query + '&f=live')
+        with webdriver.Firefox(options=opt) as driver:
+            query = input('enter query> ').replace(' ', '%20')
+            driver.get('https:/twitter.com/search?q=' + query + '&f=live')
 
-        try:
-            # basically get's all the span.text inside the tweet's div. (Autor, body, everything...)
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located(
-                    (By.XPATH, '//div[@data-testid="tweet"]')
+            try:
+                # basically get's all the span.text inside the tweet's div. (Autor, body, everything...)
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, '//div[@data-testid="tweet"]')
+                    )
                 )
-            )
 
-            tweets = driver.find_elements_by_xpath('//div[@data-testid="tweet"]')
+                tweets = driver.find_elements_by_xpath('//div[@data-testid="tweet"]')
 
-            print('Printing the contents of the first 10 tweets.')
+                print('Printing the contents of the first 10 tweets.')
 
             # iterate over the first 10 or less possible tweets.
-            for i in range(10):
-                try:
-                    print('\n*TWEET(%i) ==================' % (i + 1))
-                    spans = tweets[i].find_elements_by_tag_name('span')
-                    text  = [ ]
-                    for word in spans:
-                        text.append(word.text)
-                    print(' '.join(text))
-                    print('============================')
-                except IndexError:
-                    break
+                for i in range(10):
+                    try:
+                        print('\n*TWEET(%i) ==================' % (i + 1))
+                        spans = tweets[i].find_elements_by_tag_name('span')
+                        text  = [ ]
+                        for word in spans:
+                            text.append(word.text)
+                            print(' '.join(text))
+                            print('============================')
+                    except IndexError:
+                        break
         finally:
             driver.quit()
 except KeyboardInterrupt:
     print('\nExiting...')
-    driver.quit()
     exit(0)
